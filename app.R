46ddefa315d7b711b497e8ad322ded57fd43d6cf
XWu10 <xwu10@mdanderson.org>
2020-04-28 08:24:00 -0500

    test R Shiny dynamic UI

--- app.R
+++ app.R
@@ -6,40 +6,171 @@ library(magrittr)
 library(DESeq2)
 library(DT)
 
+# example input files
+rnaRds <- "./data/deseq2_res_list.rds"
+dnaMaf <- "./data/DNA_filtered__mutect_Pindel_GM_pipe_201002.maf"
+rnaTsv <- "./data/RNA_merged_HTseq_count.tsv"
+rppa <- "./data/IACS__RPPA.txt"
 
-source("tab_design.R")
-source("helpers.R")
-source("visualization.R")
+source("./tab_design.R")
+source("./helpers.R")
+source("./visualization.R")
 
-deseq2_res <- readRDS("./data/deseq2_res_list.rds")[[1]]
-
-
-
+# user interface
 ui <- navbarPage(
-    
     theme = shinytheme("yeti"),
-    title = "Omics Data Explorer",
+    title = "Genomic Data Explorer",
     id = "tabs",
-    
+
     tab_upload,
     tab_dna,
     tab_rna,
-    tab_scrna
+    tab_rppa,
+    tab_scrna,
+    tab_integrate
 )
 
+# server function
 server <- function(input, output) {
 
-    output$ph1 <- renderText("Placeholder")
-    output$ph2 <- renderText("Placeholder")
-    output$ph4 <- renderText("Placeholder")
-    
-    output$volcano <- renderPlot({
-        deseq_volcano(deseq2_res, input$p_co, input$lfc_co)
-    }, height = 700)
-    
-    output$table <- DT::renderDataTable({
-        deseq_table(deseq2_res, input$p_co, input$lfc_co)
+    tempMat <- reactiveValues(mat = NULL)
+    observeEvent(input$submitFiles, {
+
+        meta <- makeMetaTable(dnaMaf = input$dnafile)
+        output$specifyDnaGenes <- renderUI({
+            textInput("genes", "Name of genes", "EGFR;TP53;RB1;ALK;AML1;TERT;KRAS;PI3K")
+        })
+        output$specifyDnaSamples <- renderUI({
+            textInput("samples", "Name of samples", paste(meta[["sid"]], sep = "", collapse = ";"))
+        })
+
+        meta <- makeMetaTable(rnaTsv = input$rnafile)
+        output$specifyRnaGenes <- renderUI({
+            textInput("genes", "Name of genes", "EGFR;TP53;RB1;ALK;AML1;TERT;KRAS;PI3K")
+        })
+        output$specifyRnaSamples <- renderUI({
+            textInput("samples", "Name of samples", paste(meta[["sid"]], sep = "", collapse = ";"))
+        })
+
+        meta <- makeMetaTable(rppa = input$rppafile)
+        output$specifyRppaGenes <- renderUI({
+            textInput("genes", "Name of genes", "EGFR;TP53;RB1;ALK;AML1;TERT;KRAS;PI3K")
+        })
+        output$specifyRppaSamples <- renderUI({
+            textInput("samples", "Name of samples", paste(meta[["sid"]], sep = "", collapse = ";"))
+        })
+
+        output$ph1 <- renderText("Upload datasets successfully!")
     })
+
+    observeEvent(input$resetDnaGenesSamples, {
+        f <- as.character(input$dnafile)
+        temp <- read.delim(f, sep = "	", header = TRUE, as.is = TRUE)
+        temp <- temp[temp[, "Tumor_Sample_Barcode"] %in% unlist(strsplit(as.character(input$samples), ";")), ]
+        temp <- temp[temp[, "Hugo_Symbol"] %in% unlist(strsplit(as.character(input$genes), ";")), ]
+        tempMat$mat <- read.maf(temp)
+
+        output$waterfall <- renderPlot({
+            if(is.null(tempMat$mat)){
+                tempMat$mat <- NULL
+            }else{
+                if(class(tempMat$mat) == "MAF"){
+                    require(maftools)
+                    oncoplot(tempMat$mat, draw_titv = TRUE)
+                }
+            }
+        })
+
+        output$mafSummary <- renderPlot({
+            if(is.null(tempMat$mat)){
+                tempMat$mat <- NULL
+            }else{
+                if(class(tempMat$mat) == "MAF"){
+                    require(maftools)
+                    plotmafSummary(maf = tempMat$mat, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE)
+                }
+            }
+        })
+
+        output$vafPlot <- renderPlot({
+            if(is.null(tempMat$mat)){
+                tempMat$mat <- NULL
+            }else{
+                if(class(tempMat$mat) == "MAF"){
+                    require(maftools)
+                    plotVaf(maf = tempMat$mat, vafCol = 'VAF', top = 30)
+                }
+            }
+        })
+
+    })
+
+    observeEvent(input$resetRnaGenesSamples, {
+        f <- as.character(input$rnafile)
+        temp <- read.delim(f, sep = "	", header = TRUE, as.is = TRUE)
+        temp <- temp[temp[, "symbol"] %in% unlist(strsplit(as.character(input$genes), ";")), , drop = FALSE]
+        temp <- temp[!duplicated(temp[, "symbol"]), ]
+        rown <- temp[, "symbol"]
+        temp <- temp[, colnames(temp) %in% unlist(strsplit(as.character(input$samples), ";"))]
+        temp <- apply(apply(temp, 2, function(x) log2(as.numeric(x) + 1)), 2, function(x) (x - mean(x, na.rm = TRUE))/sd(x))
+        rownames(temp) <- rown
+        temp <- as.matrix(temp)
+        class(temp) <- "rna"
+        tempMat$mat <- temp
+
+        output$clusterRna <- renderPlot({
+            if(is.null(tempMat$mat)){
+                tempMat$mat <- NULL
+            }else{
+                if(class(tempMat$mat) == "rna"){
+                    require(gplots)
+                    cols = colorRampPalette(c("forestgreen", "yellow", "red"))(n =299)
+                    plot(hclust(dist(tempMat$mat)))
+                    heatmap.2(tempMat$mat, col = cols, tracecol = NA, dendgrogram = "column")
+                }
+            }
+        })
+
+        deseq2_res <- readRDS(input$rdsfile)[[1]]
+
+        output$volcano <- renderPlot({
+            deseq_volcano(deseq2_res, input$p_co, input$lfc_co)
+        }, height = 700)
+
+        output$table <- DT::renderDataTable({
+            deseq_table(deseq2_res, input$p_co, input$lfc_co)
+        })
+    })
+
+    observeEvent(input$resetRppaGenesSamples, {
+        f <- as.character(input$rppafile)
+        temp <- read.delim(f, sep = "	", header = TRUE, as.is = TRUE)
+        rownames(temp) <- rown <- temp[, "Sample.Name"]
+        temp <- temp[, -which(colnames(temp) == "Sample.Name")]
+        temp <- temp[, rownames(temp) %in% unlist(strsplit(as.character(input$samples), ";"))]
+        temp <- apply(apply(temp, 2, function(x) log2(as.numeric(x) + 1)), 2, function(x) (x - mean(x, na.rm = TRUE))/sd(x))
+        rownames(temp) <- rown
+        temp <- data.matrix(t(temp))
+        class(temp) <- "rppa"
+        tempMat$mat <- temp
+
+        output$clusterRppa <- renderPlot({
+            if(is.null(tempMat$mat)){
+                tempMat$mat <- NULL
+            }else{
+                if(class(tempMat$mat) == "rppa"){
+                    require(gplots)
+                    cols = colorRampPalette(c("forestgreen", "yellow", "red"))(n =299)
+                    plot(hclust(dist(tempMat$mat)))
+                    heatmap.2(tempMat$mat, col = cols, tracecol = NA, dendgrogram = "column")
+                }
+            }
+        })
+    })
+
+    output$ph4 <- renderText("Under Construction...")
+
+    output$ph5 <- renderText("Under Construction...")
 }
 
 shinyApp(ui = ui, server = server)
--- tab_design.R
+++ tab_design.R
@@ -1,66 +1,106 @@
 
 tab_upload <- tabPanel(
-    
+
     title = "Upload",
     fluid = TRUE,
     value = "v_up",
-    
+
     sidebarLayout(
-        
+
         sidebarPanel = sidebarPanel(
 
-                    h4("Please upload your data:"),
-                    fileInput(inputId = "data",
-                              label = "Data",
-                              buttonLabel = "Browse...")
+            h4("Please upload your data:"),
+            fileInput(inputId = "data",
+                      label = "Data",
+                      buttonLabel = "Browers...",
+                      placeholder = rnaRds),
+            br(),
+            h3("Data sets used:"),
+            textInput("rdsfile", "RDS data file name", rnaRds),
+            textInput("dnafile", "DNA data file name", dnaMaf),
+            textInput("rnafile", "RNA data file name", rnaTsv),
+            textInput("rppafile", "RPPA data file name", rppa),
 
+            actionButton("submitFiles", "Submit")
         ),
-        
+
         mainPanel(
             textOutput(outputId = "ph1")
         )
     )
-    
+
 )
 
 tab_dna <- tabPanel(
-    
+
     title = "DNA-seq",
     fluid = TRUE,
     value = "v_dna",
-    
+
     sidebarLayout(
-        
+
         sidebarPanel = sidebarPanel(
-            
-            
-            
+            h4("Specify genes and samples:"),
+            uiOutput("specifyDnaGenes"),
+            uiOutput("specifyDnaSamples"),
+            actionButton("resetDnaGenesSamples", "Reset")
         ),
-        
+
         mainPanel(
-            textOutput(outputId = "ph2")
+            tabsetPanel(
+                tabPanel(
+                    title = "Waterfall Plot",
+                    br(),
+                    plotOutput("waterfall")
+                ),
+                tabPanel(
+                    title = "Summary Plot",
+                    br(),
+                    plotOutput("mafSummary")
+                ),
+                tabPanel(
+                    title = "VAF Plot",
+                    br(),
+                    plotOutput("vafPlot")
+                )
+            )
         )
     )
-    
+
 )
 
 
 tab_rna <- tabPanel(
-    
+
     title = "RNA-seq",
     fluid = TRUE,
     value = "v_rna",
-    
+
     sidebarLayout(
-        
+
         sidebarPanel = sidebarPanel(
+            h4("Specify genes and samples:"),
+            uiOutput("specifyRnaGenes"),
+            uiOutput("specifyRnaSamples"),
+            actionButton("resetRnaGenesSamples", "Reset"),
+            br(),
             h4("RNA-seq significance cutoff:"),
             numericInput("p_co", label = "Adjusted p-value Cutoff", value = 0.05),
             numericInput("lfc_co", label = "Log2 Fold Change Cutoff", value = 1)
         ),
-        
+
         mainPanel(
             tabsetPanel(
+                tabPanel(
+                    title = "Clustering",
+                    br(),
+                    plotOutput("clusterRna")
+                ),
+                tabPanel(
+                    title = "PCA Plot",
+                    br(),
+                    h4("Under Construction ..."),
+                ),
                 tabPanel(
                     title = "Volcano Plot",
                     br(),
@@ -72,31 +112,132 @@ tab_rna <- tabPanel(
                     DT::dataTableOutput("table")
                 ),
                 tabPanel(
-                    title = "Functional Enrichment"
+                    title = "Functional Enrichment",
+                    br(),
+                    h4("Under Construction ...")
                 )
             )
         )
     )
-    
+
+)
+
+tab_rppa <- tabPanel(
+
+    title = "RPPA",
+    fluid = TRUE,
+    value = "v_rppa",
+
+    sidebarLayout(
+
+        sidebarPanel = sidebarPanel(
+            h4("Specify genes and samples:"),
+            uiOutput("specifyRppaGenes"),
+            uiOutput("specifyRppaSamples"),
+            actionButton("resetRppaGenesSamples", "Reset")
+        ),
+
+        mainPanel(
+            tabsetPanel(
+                tabPanel(
+                    title = "Clustering",
+                    br(),
+                    plotOutput("clusterRppa")
+                ),
+                tabPanel(
+                    title = "PCA Plot",
+                    br(),
+                    h4("Under Construction ...")
+                ),
+                tabPanel(
+                    title = "Pathway Analysis",
+                    br(),
+                    h4("Under Construction ...")
+                )
+            )
+        )
+    )
+
 )
 
 tab_scrna <- tabPanel(
-    
+
     title = "scRNA-seq",
     fluid = TRUE,
     value = "v_scrna",
-    
+
     sidebarLayout(
-        
+
         sidebarPanel = sidebarPanel(
-            
-            
-            
+
+
+
         ),
-        
+
         mainPanel(
             textOutput(outputId = "ph4")
         )
     )
-    
+
 )
+
+tab_integrate <- tabPanel(
+
+    title = "Integration",
+    fluid = TRUE,
+    value = "v_integrate",
+
+    sidebarLayout(
+
+        sidebarPanel = sidebarPanel(
+
+
+
+        ),
+
+        mainPanel(
+            textOutput(outputId = "ph5")
+        )
+    )
+
+)
+
+makeMetaTable <- function(dnaMaf, rnaTsv, rppa){
+    metaData <- NULL
+    sid <- NULL
+    if(!missing(dnaMaf)){
+        mat <- read.delim(dnaMaf, sep = "\t", header = TRUE, as.is = TRUE)
+        sid <- getSampleIds(mat, what = "DNA")
+        metaData <- rbind(metaData, c(type = "DNA", file = dnaMaf))
+    }
+    if(!missing(rnaTsv)){
+        mat <- read.delim(rnaTsv, sep = "\t", header = TRUE, as.is = TRUE)
+        sid <- getSampleIds(mat, what = "RNA")
+        metaData <- rbind(metaData, c(type = "RNA", file = rnaTsv))
+    }
+    if(!missing(rppa)){
+        mat <- read.delim(rppa, sep = "\t", header = TRUE, as.is = TRUE)
+        sid <- getSampleIds(mat, what = "RPPA")
+        metaData <- rbind(metaData, c(type = "RPPA", file = rppa))
+    }
+    if(is.null(sid)){
+        stop("DAN or RNA data set is required")
+    }
+
+    return(list(sid = sid, meta = metaData))
+}
+
+getSampleIds <- function(dataMat, what = "DNA"){
+    if(toupper(what) == "DNA"){
+        return(unique(dataMat[, "Tumor_Sample_Barcode"]))
+    }
+    if(toupper(what == "RNA")){
+        return(colnames(dataMat)[-c(1, 2)])
+    }
+    if(toupper(what == "RPPA")){
+        return(dataMat[, "Sample.Name"])
+    }
+    stop("DAN or RNA data set is required")
+
+    return(invisible())
+}
