5508513131b7bed49d814a03f6308ca2f25cf11e
XWu10 <xwu10@mdanderson.org>
2020-05-01 16:44:25 -0500

    test branch dna

--- app.R
+++ app.R
@@ -28,11 +28,11 @@ source("visualization.R")
 
 # user interface
 ui <- navbarPage(
-    
+
     theme = shinytheme("yeti"),
     title = "Genomic Data Explorer",
     id = "tabs",
-    
+
     tab_upload,
     tab_dna,
     tab_rna,
@@ -43,120 +43,111 @@ ui <- navbarPage(
 
 # server function
 server <- function(input, output) {
-    
+
     # I/O
-    
+
     tempMat <- reactiveValues(mat = NULL)
     observeEvent(input$submitFiles, {
-        
+
         meta <- makeMetaTable(dnaMaf = input$dnafile)
-        
+
         output$specifyDnaGenes <- renderUI({
             textInput("genes", "Name of genes", "EGFR;TP53;RB1;ALK;AML1;TERT;KRAS;PI3K")
         })
-        
+
         output$specifyDnaSamples <- renderUI({
             textInput("samples", "Name of samples", paste(meta[["sid"]], sep = "", collapse = ";"))
         })
-        
-        
+
+
         meta <- makeMetaTable(rppa = input$rppafile)
-        
+
         output$specifyRppaGenes <- renderUI({
             textInput("genes", "Name of genes", "EGFR;TP53;RB1;ALK;AML1;TERT;KRAS;PI3K")
         })
-        
+
         output$specifyRppaSamples <- renderUI({
             textInput("samples", "Name of samples", paste(meta[["sid"]], sep = "", collapse = ";"))
         })
-        
+
         output$ph1 <- renderText("Upload datasets successfully!")
     })
-    
-    
+
+
     # DNA
-    
+
     observeEvent(input$resetDnaGenesSamples, {
         f <- as.character(input$dnafile)
         temp <- read.delim(f, sep = "	", header = TRUE, as.is = TRUE)
         temp <- temp[temp[, "Tumor_Sample_Barcode"] %in% unlist(strsplit(as.character(input$samples), ";")), ]
         temp <- temp[temp[, "Hugo_Symbol"] %in% unlist(strsplit(as.character(input$genes), ";")), ]
         tempMat$mat <- read.maf(temp)
-        
+
         output$waterfall <- renderPlot({
             if(is.null(tempMat$mat)){
                 tempMat$mat <- NULL
             }else{
-                if(class(tempMat$mat) == "MAF"){
-                    require(maftools)
-                    oncoplot(tempMat$mat, draw_titv = TRUE)
-                }
+                oncoplot(tempMat$mat, draw_titv = TRUE)
             }
         })
-        
+
         output$mafSummary <- renderPlot({
             if(is.null(tempMat$mat)){
                 tempMat$mat <- NULL
             }else{
-                if(class(tempMat$mat) == "MAF"){
-                    require(maftools)
-                    plotmafSummary(maf = tempMat$mat, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE)
-                }
+                plotmafSummary(maf = tempMat$mat, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE)
             }
         })
-        
+
         output$vafPlot <- renderPlot({
             if(is.null(tempMat$mat)){
                 tempMat$mat <- NULL
             }else{
-                if(class(tempMat$mat) == "MAF"){
-                    require(maftools)
-                    plotVaf(maf = tempMat$mat, vafCol = 'VAF', top = 30)
-                }
+                plotVaf(maf = tempMat$mat, vafCol = 'VAF', top = 30)
             }
         })
-        
+
     })
-    
-    
+
+
     # RNA
-    
+
     output$deseq_hm <- renderPlot({
-        deseq_heatmap(rnaseq[[1]], 
+        deseq_heatmap(rnaseq[[1]],
                       input$pca_var,
                       input$palette_con,
                       input$palette_dir)
     }, height = 700, width = 800)
-    
+
     output$deseq_pca <- renderPlot({
         if (is.numeric(rnaseq[[1]]@colData[[input$pca_var]])) {
-            deseq_pca(rnaseq[[1]], 
-                      input$pca_var, 
+            deseq_pca(rnaseq[[1]],
+                      input$pca_var,
                       input$palette_con,
                       ifelse(input$palette_dir, 1, -1))
         } else {
-            deseq_pca(rnaseq[[1]], 
-                      input$pca_var, 
+            deseq_pca(rnaseq[[1]],
+                      input$pca_var,
                       input$palette_cat)
         }
     }, height = 600)
-    
+
     output$deseq_ma <- renderPlot({
         deseq_ma(rnaseq[[2]])
     }, height = 600)
-    
+
     output$deseq_volcano <- renderPlot({
         deseq_volcano(rnaseq[[2]], input$p_co, input$lfc_co)
     }, height = 600)
-    
+
     output$deseq_table <- DT::renderDataTable({
         deseq_table(rnaseq[[2]], input$p_co, input$lfc_co)
     })
-    
+
     output$deseq_gsea <- renderPlot({
         deseq_gsea(rnaseq[[2]])
     }, height = 600)
-    
+
 
     # RPPA
 
@@ -188,7 +179,7 @@ server <- function(input, output) {
 
     output$ph4 <- renderText("Under Construction...")
     output$ph5 <- renderText("Under Construction...")
-    
+
 }
 
 shinyApp(ui = ui, server = server)
